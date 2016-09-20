e474dfb81281ebed5af3b9a0ff52606bd51c70c8
whgrimshaw <WilliamHenryGrimshaw@gmail.com>
2016-09-20 18:53:17 +0100

    Added connection to database

--- RGSTRconnect.py
+++ RGSTRconnect.py
@@ -1,3 +1,13 @@
-if __name__ == "__main__":
-    #kweuWEHwef
-    
+import mysql.connector
+def databaseconnect():
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='rgstr',
+                                       user='root',
+                                       password='root')
+        if conn.is_connected():
+            print('Connected to database successfully')
+ 
+    except Error as e:
+        print("Connection failed")
+        print(e)
--- RGSTRmain.py
+++ RGSTRmain.py
@@ -1,11 +1,13 @@
 import time
 import calendar
+import mysql.connector
+from RGSTRconnect import databaseconnect
 from time import sleep
 
 
 
 if __name__ == "__main__":
-    while True:
-        sleep(1)
+    databaseconnect()
+
         
     
