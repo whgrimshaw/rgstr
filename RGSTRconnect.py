5647b0e54b386e7b7d9c10aed3a0160ddc301df7
whgrimshaw <WilliamHenryGrimshaw@gmail.com>
2016-09-20 20:44:16 +0100

    added external config file to database connection file
    allows user to easily edit database information

--- RGSTRconnect.py
+++ RGSTRconnect.py
@@ -1,13 +1,28 @@
-import mysql.connector
-def databaseconnect():
+from mysql.connector import MySQLConnection, Error
+from configparser import ConfigParser
+
+def readconfig(configfile,section='mysql'):
+    parser=ConfigParser()
+    parser.read(configfile)
+    db={}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        print("Error reading config file- section doesnt exist")
+    return (db)
+
+def databaseconnect(configfile):
+    db_config = readconfig(configfile)
     try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='rgstr',
-                                       user='root',
-                                       password='root')
-        if conn.is_connected():
-            print('Connected to database successfully')
+        print('Connecting to MySQL database...')
+        conn = MySQLConnection(**db_config)
  
-    except Error as e:
-        print("Connection failed")
-        print(e)
+        if conn.is_connected():
+            print('connection established.')
+        else:
+            print('connection failed.')
+ 
+    except Error as error:
+        print(error)
--- RGSTRmain.py
+++ RGSTRmain.py
@@ -1,13 +1,14 @@
 import time
 import calendar
 import mysql.connector
-from RGSTRconnect import databaseconnect
+from RGSTRconnect import *
 from time import sleep
 
 
 
 if __name__ == "__main__":
-    databaseconnect()
-
+    configfile="config.ini"
+    databaseconnect(configfile)
+    
         
     
